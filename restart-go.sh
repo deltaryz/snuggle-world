744ec1566f9ee76bee4147fcb84f9611a3bd1d48
techniponi <wincam97@gmail.com>
2017-08-25 20:14:33 -0500

    Prep discord bot for usage with temp command

--- restart-go.sh
+++ restart-go.sh
@@ -2,4 +2,4 @@
 
 screen -S goserv -X kill
 chmod +x snuggle-world
-screen -S goserv -dm ./snuggle-world
+screen -S goserv -dm ./snuggle-world -t $SUNBOT_KEY
--- snuggle.go
+++ snuggle.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// global
+var dg *discordgo.Session
+
+var (
+	// Token : Discord API bot token
+	Token string
+)
+
+func init() {
+
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+
+}
 
 func main() {
-	fmt.Println("placeholder ayyyy")
+
+	// Create a new Discord session using the provided bot token (commandline arg).
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Message handler
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called every time a new message is created
+// on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	cmdToken := "." // TODO: move to external config
+
+	if m.Content[:1] == cmdToken {
+		messageArgs := strings.Split(m.Content[1:], " ")
+		switch messageArgs[0] {
+		case "butts":
+			sendMessage(m, "that's hot")
+			break
+		}
+	}
+
+}
+
+// Sends a message to the same server as the given message ID
+// (this makes repeated usage take less typing)
+func sendMessage(msg *discordgo.MessageCreate, res string) {
+	dg.ChannelMessageSend(msg.ChannelID, res)
 }
